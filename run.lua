9d3f34879b3e59fab57abd06c7a237803692ccce
BuildTools <unconfigured@null.spigotmc.org>
2023-05-14 16:03:50 +0200

    added info transmission

--- run.lua
+++ run.lua
@@ -2,8 +2,11 @@
 local reactorSide = "bottom"
 local wasteLimit = 10
 local tempLimit = 600
+local recvChan = 21
+local sendChan = 22
 
 local reactor = peripheral.wrap(reactorSide)
+local modem = peripheral.find("modem")
 
 while true do
     if reactor.getStatus() then
@@ -16,6 +19,17 @@ while true do
             print("Not enough coolant ! ABORT SYSTEM")
         end
     end
+
+    -- Transmit msg
+    local msg = {
+        fuel = reactor.getFuel().amount,
+        coolant = reactor.getCoolant().amount,
+        temp = reactor.getTemperature(),
+        waste = reactor.getWaste().amount,
+        status = reactor.getStatus()
+
+    }
+    modem.transmit(sendChan, recvChan, msg)
     sleep(1)
 end
 
